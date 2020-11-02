8f956cc7fa9fd6907021560cd27538f6054bc606
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2020-11-02 14:08:19 +0900

    [fe] [feat]: 로컬에서 라우팅을 위한 설정 추가
    - 로컬에서 dev로 빌드할 경우 github 인증 후 callback url에서 404 에러 발생
    - callback 라우팅을 위한 설정 추가

--- frontend/webpack.config.dev.js
+++ frontend/webpack.config.dev.js
@@ -4,7 +4,11 @@ const common = require('./webpack.config.common.js');
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
+  output: {
+    publicPath: '/',
+  },
   devServer: {
     contentBase: './dist',
+    historyApiFallback: true,
   },
 });
