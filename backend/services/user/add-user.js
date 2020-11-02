3d3a1cc40d37a1391b6e25dc27ad8c90305d0921
mjseok <mjseok98@gmail.com>
2020-11-02 21:52:47 +0900

    [BE][feat]:저장되어있는 user인지 확인후 생성
    sequelize의 findOrCreate를 이용해서 존재하는 user의 정보가 아닐 경우에만
    db에 추가

--- backend/services/user/add-user.js
+++ backend/services/user/add-user.js
@@ -1,16 +1,15 @@
 const { user } = require('../../sequelize/models');
 
 module.exports = (req, res) => {
-  try {
-    user
-      .create({ local_id: req.data.id, nickName: req.data.login, provider: 'GitHub' })
-      .then((results) => {
-        res.send(results);
-      })
-      .catch((err) => {
-        res.status(500).send(err);
-      });
-  } catch (e) {
-    res.json({ ok: false });
-  }
+  user
+    .findOrCreate({
+      where: { local_id: req.data.id },
+      defaults: { local_id: req.data.id, nickName: req.data.login, provider: 'GitHub' },
+    })
+    .then((results) => {
+      res.send(results);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
 };
