4d6001e18ccadf5bc89005c664ed2e7164573689
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2020-11-02 15:08:15 +0900

    [fe] [feat]: 로그인 페이지 스타일 적용
    - styled components 적용
    - github icon 삽입을 위한 file-loader 추가
    - 로그인 페이지 style 적용

--- frontend/components/Login.jsx
+++ frontend/components/Login.jsx
@@ -1,10 +1,72 @@
 import React from 'react';
+import styled from 'styled-components';
+import GithubIconImage from '@static/github-icon-48.png';
 
 const Login = () => {
   const endpoint = 'https://github.com/login/oauth/authorize';
   const GITHUB_CLIENT_ID = 'f7b2106d984fcad19336';
 
-  return <a href={`${endpoint}?client_id=${GITHUB_CLIENT_ID}`}>로그인하기</a>;
+  return (
+    <Wrapper>
+      <Title>Issue Tracker</Title>
+      <LoginWrapper>
+        <GithubLoginButton href={`${endpoint}?client_id=${GITHUB_CLIENT_ID}`}>
+          <div>Github로 로그인하기</div>
+          <GithubIcon src={GithubIconImage} alt='github icon' />
+        </GithubLoginButton>
+      </LoginWrapper>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+const Title = styled.div`
+  font-size: 2.5rem;
+  font-weight: bold;
+  margin: 1.5rem;
+`;
+
+const LoginWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 10px 20px;
+  border: 1px solid gray;
+  border-radius: 5px;
+`;
+
+const GithubLoginButton = styled.a`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  width: 20rem;
+  height: 2.2rem;
+  background-color: rgba(104, 104, 104, 0.8);
+  border-radius: 5px;
+  font-weight: bold;
+  text-decoration: none;
+  &:visited {
+    color: inherit;
+  }
+  div {
+    color: white;
+  }
+`;
+
+const GithubIcon = styled.img`
+  width: 1.8rem;
+  margin: 0 0.5rem;
+`;
 
 export default Login;
