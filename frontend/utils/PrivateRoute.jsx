3cb3e1f163b2262c0ca6f26a48040c95f239acdb
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2020-11-02 21:16:43 +0900

    [fe][feat] : 유저 라우팅 제한 #10
    - isPending과 isAuthorized state 활용
    - isPending이 true일 경우 빈 페이지를 보여줌
      - 이후 checkUserAuth를 실행
      - 콜백으로 state 변경 함수 전달
    - isPending 변경 시(유효성 확인 완료 시)
      - 유효한 유저인 경우 PrivateRoute로 이동하거나 로그인 페이지 접근 방지
      - 유효하지 않은 유저의 경우 로그인 페이지로 이동

--- frontend/utils/PrivateRoute.jsx
+++ frontend/utils/PrivateRoute.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import userAuthenticate from '@utils/user-authenticate';
 
 /**
  *  PrivateRoute
@@ -8,21 +9,26 @@ import PropTypes from 'prop-types';
  *  그 외에는 Route 컴포넌트와 같습니다.
  */
 const PrivateRoute = ({ component, ...rest }) => {
+  const [isAuthorized, setIsAuthorized] = useState(userAuthenticate.isAuthorized);
+  const [isPending, setIsPending] = useState(userAuthenticate.isPending);
+
+  useEffect(() => {
+    if (isPending) userAuthenticate.checkUserAuth(setIsAuthorized, setIsPending);
+  }, []);
+
+  if (isPending) return <>확인 중</>;
+  if (isAuthorized) return <Route {...rest} render={() => React.createElement(component)} />;
   return (
     <Route
       {...rest}
-      render={({ location }) =>
-        true ? ( // TODO: true/false 자리에 로그인 여부를 판단하는 조건으로 교체
-          React.createElement(component)
-        ) : (
-          <Redirect
-            to={{
-              pathname: '/',
-              state: { from: location },
-            }}
-          />
-        )
-      }
+      render={({ location }) => (
+        <Redirect
+          to={{
+            pathname: '/',
+            state: { from: location },
+          }}
+        />
+      )}
     />
   );
 };
