6a4b9cec3ea87e0650f7a0e3c5cc53bc3edaa0ac
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2020-11-02 13:57:05 +0900

    [fe] [feat]: 유저 인증을 위한 라우팅 추가
    - '/' 경로로 접근 시 Login 컴포넌트로 이동
    - github 인증 후 callback url 호출 시 LoggingIn  컴포넌트로 이동

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Login from './Login';
+import LoggingIn from './LoggingIn';
 
-const App = () => <>Hello World!</>;
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path='/' component={Login} />
+      <Route path='/oauth/callback/github' component={LoggingIn} />
+    </Switch>
+  </BrowserRouter>
+);
 
 export default App;
