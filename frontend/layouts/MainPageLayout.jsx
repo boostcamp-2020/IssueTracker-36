22800ea1b36228891b6174e4ad3bd1fa28541c81
mjseok <mjseok98@gmail.com>
2020-11-03 18:00:19 +0900

    [FE][feat] :Header 컴포넌트 작성 #25
    Header부분 MainPageLayout에 추가

--- frontend/layouts/MainPageLayout.jsx
+++ frontend/layouts/MainPageLayout.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import GithubIconImage from '@static/github-icon-48.png';
 
 const MainPageLayout = ({ children }) => {
   return (
     <Container>
+      <Header>
+        <GithubLogo src={GithubIconImage} alt='github icon' />
+        <IssueButton href='/issues'>Issues</IssueButton>
+        <LogoutButton href='/'>로그아웃</LogoutButton>
+      </Header>
       <Main>{children}</Main>
     </Container>
   );
@@ -18,9 +24,43 @@ const Container = styled.div`
   width: 100%;
   height: 100%;
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
   background-color: grey;
+`;
+const Header = styled.div`
+  width: 100%;
+  height: 10%;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  background-color: black;
+`;
+const GithubLogo = styled.img`
+  width: 3rem;
+  margin: 0 0.5rem;
+  filter: invert(100%);
+`;
+const IssueButton = styled.a`
+  color: white;
+  font-size: 30px;
+  text-decoration: none;
+`;
+const LogoutButton = styled.button`
+  width: 10%;
+  height: 40%;
+  color: black;
+  font-weight: bold;
+  line-height: 20px;
+  font-size: 15px;
+  border: transparent;
+  border-radius: 4px;
+  outline: 0;
+  &:hover {
+    background-color: white;
+    color: gray;
+  }
 `;
 
 const Main = styled.main`
