4a46c08e700eeac5a4cb3fa41f46d5843621556c
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2020-11-03 15:37:23 +0900

    [FE][feat] : 유저 로그인 동작 수정
    - 유저의 로그인 정보, 이름을 저장하는 객체를 별도로 생성
    - authorized 속성은 우선 유저의 토큰이 있는지 여부만 확인
      - 유효성 검사는 api 요청 시 서버에서 진행할 예정
    - 로그인할 경우 login 함수를 실행해 속성 변경

--- frontend/pages/auth/LoginPage.jsx
+++ frontend/pages/auth/LoginPage.jsx
@@ -1,29 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import { Redirect } from 'react-router-dom';
-import PropTypes from 'prop-types';
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
 import styled from 'styled-components';
-import userAuthenticate from '@utils/user-authenticate';
+import userInfo from '@utils/user-info';
 import GithubIconImage from '@static/github-icon-48.png';
 
 const LoginPage = () => {
-  const [isAuthorized, setIsAuthorized] = useState(userAuthenticate.isAuthorized);
-  const [isPending, setIsPending] = useState(userAuthenticate.isPending);
   const endpoint = 'https://github.com/login/oauth/authorize';
   const GITHUB_CLIENT_ID = 'f7b2106d984fcad19336';
 
-  useEffect(() => {
-    if (isPending) userAuthenticate.checkUserAuth(setIsAuthorized, setIsPending);
-  }, []);
-  if (isPending) return <></>;
-  if (isAuthorized)
-    return (
-      <Redirect
-        to={{
-          pathname: '/issues',
-        }}
-      />
-    );
-  return (
+  return userInfo.authorized ? (
+    <Route
+      render={({ location }) => (
+        <Redirect
+          to={{
+            pathname: '/issues',
+            state: { from: location },
+          }}
+        />
+      )}
+    />
+  ) : (
     <Wrapper>
       <Title>Issue Tracker</Title>
       <LoginWrapper>
--- frontend/utils/PrivateRoute.jsx
+++ frontend/utils/PrivateRoute.jsx
@@ -1,24 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import userAuthenticate from '@utils/user-authenticate';
+import userInfo from '@utils/user-info';
 
 /**
  *  PrivateRoute
  *  로그아웃 상태일 때는 로그인페이지로 Redirect 시킵니다.
  *  그 외에는 Route 컴포넌트와 같습니다.
  */
-const PrivateRoute = ({ component, ...rest }) => {
-  const [isAuthorized, setIsAuthorized] = useState(userAuthenticate.isAuthorized);
-  const [isPending, setIsPending] = useState(userAuthenticate.isPending);
-
-  useEffect(() => {
-    if (isPending) userAuthenticate.checkUserAuth(setIsAuthorized, setIsPending);
-  }, []);
-
-  if (isPending) return <>확인 중</>;
-  if (isAuthorized) return <Route {...rest} render={() => React.createElement(component)} />;
-  return (
+const PrivateRoute = ({ component, ...rest }) =>
+  userInfo.authorized ? (
+    <Route {...rest} render={() => React.createElement(component)} />
+  ) : (
     <Route
       {...rest}
       render={({ location }) => (
@@ -31,7 +24,6 @@ const PrivateRoute = ({ component, ...rest }) => {
       )}
     />
   );
-};
 
 PrivateRoute.propTypes = {
   component: PropTypes.func.isRequired,
