4634672d4e9eb7aee5bd08665b71c5611781eb6c
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2020-11-03 11:23:55 +0900

    [FE][feat] : 토큰 저장 #10
    - 서버에서 전달한 토큰을 local storage에 저장

--- frontend/services/auth/oauth-github.js
+++ frontend/services/auth/oauth-github.js
@@ -2,8 +2,10 @@ import axios from 'axios';
 
 const oauthGithub = async (code) => {
   const endpoint = 'http://localhost:3000/api/auth/oauth/github';
-  const response = await axios.post(endpoint, { code });
-  return response;
+  const {
+    data: { token },
+  } = await axios.post(endpoint, { code });
+  window.localStorage.setItem('userToken', token);
 };
 
 export default oauthGithub;
