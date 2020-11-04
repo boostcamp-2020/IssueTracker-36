a3ba9ead736bae31584ecc2059ecb7a99da1c279
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2020-11-04 14:29:07 +0900

    [FE][refactor] : LabelMilestone페이지 폴더 변경 #51 - LabelMilestonePage 이동에 따른 page index 수정

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -3,7 +3,7 @@ export { default as IssueDetailPage } from '@pages/issues/IssueDetailPage';
 export { default as IssueListPage } from '@pages/issues/IssueListPage';
 export { default as LoginPage } from '@pages/auth/LoginPage';
 export { default as LoggingInPage } from '@pages/auth/LoggingInPage';
-export { default as LabelMilestonePage } from '@pages/LabelMilestonePage';
+export { default as LabelMilestonePage } from '@pages/common/LabelMilestonePage';
 export { default as MilestoneAddPage } from '@pages/milestones/MilestoneAddPage';
 export { default as MilestoneDetailPage } from '@pages/milestones/MilestoneDetailPage';
 export { default as SignupPage } from '@pages/auth/SignupPage';
