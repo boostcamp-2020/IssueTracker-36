57733cec0e8f45a97f37dc6445cd541f6ce1febe
mjseok <mjseok98@gmail.com>
2020-11-05 02:17:54 +0900

    [FE][feat] : milestone service 추가
    milestones관련 service 추가

--- frontend/services/index.js
+++ frontend/services/index.js
@@ -1,10 +1,12 @@
 import apiRequest from '@utils/api-request';
 import labelService from './label';
+import MilestoneService from './milestones/index';
 // import auth from './auth';
 
 const Service = () => {
   return {
     ...labelService(apiRequest),
+    ...MilestoneService(apiRequest),
     // ...auth(),
   };
 };
