2713c3f9777aed8a7fec7a31aa9ca1a789deb206
dogyeong <shroad1802@naver.com>
2020-11-04 11:06:16 +0900

    [fe][feat] : storybook 환경설정 수정 #28
    - example code를 stories 폴더 내로 이동

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
-  stories: ['../components/**/*.stories.mdx', '../components/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
 };
--- frontend/stories/examples/Button.jsx
+++ frontend/stories/examples/Button.jsx
@@ -9,7 +9,7 @@ export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
   const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
   return (
     <button
-      type="button"
+      type='button'
       className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
       style={backgroundColor && { backgroundColor }}
       {...props}
