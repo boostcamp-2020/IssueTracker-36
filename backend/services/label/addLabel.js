26f426d64786e5953d057c4a9959bea84605fe4a
mjseok <mjseok98@gmail.com>
2020-11-05 17:01:54 +0900

    [BE][feat] :color값 유효성 검사 #78
    정규표현식을 이용해서 color값이 유효 검사
    유효하지 않으면 typeerror

--- backend/services/label/addLabel.js
+++ backend/services/label/addLabel.js
@@ -2,14 +2,15 @@ const { label } = require('../../sequelize/models');
 
 const addLabel = async (req, res) => {
   try {
-    const generatedLabel = await label.create({
-      title: req.body.title,
-      description: req.body.description,
-      color: req.body.color,
-    });
+    const { title, description, color } = req.body;
+    const colorRegex = /^#[0-9a-f]{6}$/i;
+    if (!colorRegex.test(color)) throw new TypeError();
+
+    const generatedLabel = await label.create({ title, description, color });
     res.json(generatedLabel);
-  } catch (e) {
-    res.sendStatus(500);
+  } catch (err) {
+    if (err instanceof TypeError) res.sendStatus(400);
+    else res.sendStatus(500);
   }
 };
 
