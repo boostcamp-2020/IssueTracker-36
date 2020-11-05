074ce9fe14d0bcb9547894bfb7a814289078fde6
saeeng <saeeng.op@gmail.com>
2020-11-05 16:39:06 +0900

    [FE][fix] mainlayout responsive 적용
    - 메인레이아웃 넓이 조절
    - 미디어 쿼리 활용하여 화면 사이즈에 따른 패딩조절

--- frontend/layouts/MainPageLayout.jsx
+++ frontend/layouts/MainPageLayout.jsx
@@ -23,11 +23,26 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
   background-color: ${({ theme }) => theme.color.whiteColor};
+
+  margin-right: auto;
+  margin-left: auto;
 `;
 const Main = styled.main`
-  width: 100%;
-  max-width: 1024px;
   margin-top: 3rem;
+  max-width: 1280px;
+  width: 100%;
+  @media (min-width: 1012px) {
+    padding-right: 32px !important;
+  }
+  @media (min-width: 1012px) {
+    padding-left: 32px !important;
+  }
+  @media (min-width: 768px) {
+    padding-right: 24px !important;
+  }
+  @media (min-width: 768px) {
+    padding-left: 24px !important;
+  }
 `;
 
 export default MainPageLayout;
