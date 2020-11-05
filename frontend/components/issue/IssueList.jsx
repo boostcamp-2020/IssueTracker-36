3fef11c39037effc080249a519bb97441516b427
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2020-11-05 15:32:46 +0900

    [FE][feat] : 라벨, 마일스톤 정보 표시 #77 - issueList에서 IssueCard로 전달하는 props 추가   - Label을 생성해 array로 전달   - milestone 이름을 전달

--- frontend/components/issue/IssueList.jsx
+++ frontend/components/issue/IssueList.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import IssueCard from '@components/IssueCard';
+import Label from '@components/common/label';
 
 const IssueList = ({ issues }) => {
   return (
@@ -9,9 +10,16 @@ const IssueList = ({ issues }) => {
         return (
           <IssueCard
             title={issue.title}
+            labelList={issue.issue_labels.reduce((acc, issueLabels) => {
+              acc.push(
+                <Label text={issueLabels.label.title} bg={issueLabels.label.color} key={issueLabels.id} />,
+              );
+              return acc;
+            }, [])}
             issueId={issue.id}
             created={issue.createdAt}
             userNickname=''
+            milestoneName={issue.milestone.title}
             isClosed={issue.isClosed}
             key={issue.id}
           />
