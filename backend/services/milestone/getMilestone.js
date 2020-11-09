d5e5531fd74766a73deb5ae27d30c4841b6ea9d7
mjseok <mjseok98@gmail.com>
2020-11-09 22:24:54 +0900

    [BE][feat] : 단일 마일스톤 조회 api #104
    단일 마일스톤 조회 api
    해당 마일스톤 정보, 관련 issue 받아오기

--- backend/services/milestone/getMilestone.js
+++ backend/services/milestone/getMilestone.js
@@ -1,10 +1,15 @@
-const { milestone } = require('../../sequelize/models');
+const { milestone, issue } = require('../../sequelize/models');
 
 module.exports = async (req, res) => {
   try {
     const { id } = req.params;
     const result = await milestone.findOne({
       where: { id },
+      include: [
+        {
+          model: issue,
+        },
+      ],
     });
     res.json(result);
   } catch (e) {
