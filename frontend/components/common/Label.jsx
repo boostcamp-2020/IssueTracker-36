cee4d920a83c4ae67dd953cbc158e44163f457b9
mjseok <mjseok98@gmail.com>
2020-11-09 16:19:06 +0900

    [FE][refactor] : radius 통일
    라벨의 radius는 통일하기로하고 수정

--- frontend/components/common/Label.jsx
+++ frontend/components/common/Label.jsx
@@ -4,8 +4,8 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import fontColorContrast from 'font-color-contrast';
 
-const Label = ({ text, color, bg, radius, icon }) => (
-  <LabelWrapper color={color || fontColorContrast(bg)} bg={bg || 'red'} radius={radius || '2em'}>
+const Label = ({ text, color, bg, icon }) => (
+  <LabelWrapper color={color || fontColorContrast(bg)} bg={bg || 'red'} >
     {icon && <Icon>{icon}</Icon>}
     {text}
   </LabelWrapper>
@@ -15,13 +15,11 @@ Label.propTypes = {
   text: PropTypes.string.isRequired,
   color: PropTypes.string,
   bg: PropTypes.string,
-  radius: PropTypes.string,
   icon: PropTypes.element,
 };
 Label.defaultProps = {
   bg: undefined,
   color: undefined,
-  radius: undefined,
   icon: null,
 };
 
@@ -38,7 +36,7 @@ const LabelWrapper = styled.div`
   font-weight: 600;
   line-height: 22px !important;
   border: 1px solid transparent;
-  border-radius: ${(props) => props.radius || '2em'};
+  border-radius: '2em';
   font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif, Apple Color Emoji,
     Segoe UI Emoji;
 `;
