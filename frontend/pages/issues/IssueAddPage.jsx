30281e4b64c7e587edc9435983968d88560c6018
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2020-11-09 12:26:10 +0900

    [FE][feat] : 이슈 추가 페이지 컴포넌트 분리 - NewIssueForm과 IssueSidebar로 분리

--- frontend/pages/issues/IssueAddPage.jsx
+++ frontend/pages/issues/IssueAddPage.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
+import styled from 'styled-components';
 import MainPageLayout from '@layouts/MainPageLayout';
+import NewIssueForm from '@components/issue/NewIssueForm';
+import IssueSidebar from '@components/issue/IssueSidebar';
 
 const IssueAddPage = () => {
   return (
     <MainPageLayout>
-      <></>
+      <Wrapper>
+        <FormWrapper>
+          <NewIssueForm />
+        </FormWrapper>
+        <SidebarWrapper>
+          <IssueSidebar />
+        </SidebarWrapper>
+      </Wrapper>
     </MainPageLayout>
   );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const FormWrapper = styled.div`
+  flex: 3;
+`;
+
+const SidebarWrapper = styled.div`
+  flex: 1;
+  margin-left: 15px;
+  border: 1px solid red;
+`;
 
 export default IssueAddPage;
