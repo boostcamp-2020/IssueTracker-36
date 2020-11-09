5eaed51721a35f9fbf4d0479189829614b4ca156
mjseok <mjseok98@gmail.com>
2020-11-09 21:03:54 +0900

    [FE][feat] : 이슈 상세 page
    이슈 상세 페이지 header부분 component로 분리
    이슈 상세페이지 기본 틀

---
+++ frontend/components/issue/issueDetailHeader.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import Label from '@components/common/Label';
+import Moment from 'react-moment';
+import { GoIssueOpened, GoIssueClosed } from 'react-icons/go';
+import Button from '@components/common/Button';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const IssueDetailHeader = ({ issue }) => {
+  const { title, id, isClosed, createdAt } = issue;
+  const user = issue.user_issues ? issue.user_issues[0].user.nickName : '';
+  const comment = issue.comment ? issue.comment.length : 0;
+  return (
+    <>
+      <IssueHeader>
+        <IssueTitle>
+          {title}
+          <IssueNumber>{` #${id}`}</IssueNumber>
+          <IssueInfo>
+            {!isClosed ? (
+              <Label bg='green' text='Open' icon={<GoIssueOpened />} />
+            ) : (
+              <Label bg='red' text='Closed' icon={<GoIssueClosed />} />
+            )}
+            {`${user} opened this issue `}
+            <Moment fromNow>{createdAt}</Moment>
+            {` · ${comment} comment`}
+          </IssueInfo>
+        </IssueTitle>
+        <ButtonWrapper>
+          <Button text='Edit' size='large' type='secondary' />
+        </ButtonWrapper>
+      </IssueHeader>
+    </>
+  );
+};
+IssueDetailHeader.propTypes = {
+  issue: PropTypes.oneOfType([PropTypes.array, PropTypes.object]).isRequired,
+};
+const IssueTitle = styled.div`
+  font-size: ${({ theme }) => theme.fontSize.xl};
+  padding: 18px;
+`;
+const IssueNumber = styled.span`
+  color: ${({ theme }) => theme.color.grayColor};
+`;
+const IssueInfo = styled.div`
+  color: #696969;
+  font-size: ${({ theme }) => theme.fontSize.sm};
+`;
+const IssueHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  border: 1px solid ${({ theme }) => theme.color.borderColor};
+`;
+const ButtonWrapper = styled.div`
+  padding: 18px;
+  margin: auto 0;
+`;
+export default IssueDetailHeader;
--- frontend/pages/issues/IssueDetailPage.jsx
+++ frontend/pages/issues/IssueDetailPage.jsx
@@ -2,52 +2,34 @@ import React, { useState, useEffect } from 'react';
 import MainPageLayout from '@layouts/MainPageLayout';
 import service from '@services';
 import styled from 'styled-components';
-import Label from '@components/common/Label';
-import Moment from 'react-moment';
-import { GoIssueOpened, GoIssueClosed } from 'react-icons/go';
+import WritingArea from '@components/common/WritingArea';
+import IssueDetailHeader from '@components/issue/issueDetailHeader';
 
 const IssueDetailPage = (id) => {
   const [issue, setissueInfo] = useState([]);
   const getIssue = async () => {
-    const issueInfo = await service.getIssue(id);
-    setissueInfo(issueInfo);
+    const issueInfo = await service.getIssue(2);
+    setissueInfo(issueInfo.data);
+    console.log(issueInfo.data.isClosed);
   };
-  useEffect(async () => {
+  useEffect(() => {
     getIssue();
   }, []);
 
   return (
     <MainPageLayout>
-      <IssueTitle>
-        {issue.title}#{issue.id}
-        <br />
-        <IssueInfo>
-          {issue.isCloed ? (
-            <Label bg='green' text='Open' icon={<GoIssueOpened />} />
-          ) : (
-            <Label bg='red' text='Closed' icon={<GoIssueClosed />} />
-          )}
-          {` opened this issue `}
-          <Moment fromNow>{issue.createdAt}</Moment>
-          {` 숫자 comment `}
-        </IssueInfo>
-      </IssueTitle>
+      <IssueDetailHeader issue={issue} />
       <IssueDetail>
-        <IssueComment>본문,댓글</IssueComment>
+        <IssueComment>
+          본문,댓글
+          <WritingArea />
+        </IssueComment>
         <IssueSide>사이드</IssueSide>
       </IssueDetail>
     </MainPageLayout>
   );
 };
 
-const IssueTitle = styled.div`
-  border: 1px solid ${({ theme }) => theme.color.borderColor};
-  font-size: ${({ theme }) => theme.fontSize.xl};
-`;
-const IssueInfo = styled.div`
-  color: #696969;
-  font-size: ${({ theme }) => theme.fontSize.sm};
-`;
 const IssueDetail = styled.div`
   display: flex;
   flex-direction: row;
@@ -57,13 +39,13 @@ const IssueDetail = styled.div`
 const IssueComment = styled.div`
   display: flex;
   flex-direction: column;
-  width: 80%;
+  flex: 7;
   border: 3px solid ${({ theme }) => theme.color.borderColor};
 `;
 const IssueSide = styled.div`
   display: flex;
   flex-direction: column;
-  width: 20%;
+  flex: 3;
   border: 3px solid ${({ theme }) => theme.color.borderColor};
 `;
 export default IssueDetailPage;
