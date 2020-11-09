644e85a5003f5255c1dfca85abfec2f3a5539601
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2020-11-09 15:44:03 +0900

    [FE][feat] : 선택 관리 reducer 추가 #64 - 각 선택을 업데이트 할 수 있는 reducer 추가 - 해당 reducer를 사이드바에 전달 - sidebar에서는 선택 유무에 따라 뷰를 보여줌

--- frontend/components/issue/IssueSidebar.jsx
+++ frontend/components/issue/IssueSidebar.jsx
@@ -1,7 +1,50 @@
 import React from 'react';
+import styled from 'styled-components';
+import { RiSettings4Line } from 'react-icons/ri';
 
-const IssueSidebar = () => {
-  return <>Sidebar</>;
+const IssueSidebar = ({ currentSelect, chageSelect }) => {
+  return (
+    <>
+      <Wrapper>
+        <Title>
+          <p>Assignee</p>
+          <RiSettings4Line />
+        </Title>
+        {currentSelect.user ? '' : 'No one'}
+      </Wrapper>
+      <Wrapper>
+        <Title>
+          <p>Label</p>
+          <RiSettings4Line />
+        </Title>
+        {currentSelect.label ? '' : 'None yet'}
+      </Wrapper>
+      <Wrapper>
+        <Title>
+          <p>Milestone</p>
+          <RiSettings4Line />
+        </Title>
+        {currentSelect.label ? '' : 'No milestone'}
+      </Wrapper>
+    </>
+  );
 };
+
+const Wrapper = styled.div`
+  padding: 20px 0;
+  border-bottom: 1px solid gray;
+  font-size: ${({ theme }) => theme.fontSize.sm};
+`;
+
+const Title = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: 10px;
+  cursor: pointer;
+  &:hover {
+    color: ${({ theme }) => theme.color.blueColor};
+  }
+`;
 
 export default IssueSidebar;
--- frontend/pages/issues/IssueAddPage.jsx
+++ frontend/pages/issues/IssueAddPage.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import styled from 'styled-components';
 import MainPageLayout from '@layouts/MainPageLayout';
 import NewIssueForm from '@components/issue/NewIssueForm';
 import IssueSidebar from '@components/issue/IssueSidebar';
 
+const selectReducer = (state, action) => {
+  switch (action.type) {
+    case 'assignee':
+      return { ...state, user: action.newSelction };
+    case 'label':
+      return { ...state, labels: action.newSelction };
+    case 'milestone':
+      return { ...state, milestone: action.newSelction };
+    default:
+      return state;
+  }
+};
+
 const IssueAddPage = () => {
+  const [currentSelect, dispatch] = useReducer(selectReducer, {
+    assignees: [],
+    labels: [],
+    milestone: undefined,
+  });
+
   return (
     <MainPageLayout>
       <Wrapper>
@@ -12,7 +31,7 @@ const IssueAddPage = () => {
           <NewIssueForm />
         </FormWrapper>
         <SidebarWrapper>
-          <IssueSidebar />
+          <IssueSidebar currentSelect={currentSelect} chageSelect={dispatch} />
         </SidebarWrapper>
       </Wrapper>
     </MainPageLayout>
