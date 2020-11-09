66500714a8e0931713faf5ce92a95d8388c04ef9
saeeng <saeeng.op@gmail.com>
2020-11-09 17:27:29 +0900

    [BE][fix] 오타수정

--- backend/services/issue/addIssueUser.js
+++ backend/services/issue/addIssueUser.js
@@ -1,9 +1,9 @@
-const { user_issue } = require('../../sequelize/models');
+const { user_issue: userIssue } = require('../../sequelize/models');
 
 const addLabel = async (req, res) => {
   try {
     const { issue_id: issueId, user_id: userId } = req.params;
-    const result = await user_issue.create({ issueId, userId, is_owner: 0 });
+    const result = await userIssue.create({ issueId, userId, is_owner: 0 });
     res.json(result);
   } catch (err) {
     console.log(err);
