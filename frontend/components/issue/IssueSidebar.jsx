63ade5e5fe7270d16a2b405ddbabc289127fe927
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2020-11-09 17:40:22 +0900

    [FE][feat] : 사이드바 assignee, label 동작 #64 - 각 영역 클릭 시 dropdown을 보여줌 - dropdown에 이미 선택된 요소 전달 - 바뀔 경우 상위 page에서 받은 dispatch 실행

--- frontend/components/issue/IssueSidebar.jsx
+++ frontend/components/issue/IssueSidebar.jsx
@@ -1,36 +1,109 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import Dropdown from '@components/common/Dropdown';
+import optionGenerator from '@utils/OptionGenerator';
+import service from '@services';
 import { RiSettings4Line } from 'react-icons/ri';
 
 const IssueSidebar = ({ currentSelect, chageSelect }) => {
+  const [dropdown, setDropdown] = useState('');
+  const [options, setOptions] = useState([]);
+  const toggleDropdown = () => {
+    setDropdown('');
+    setOptions([]);
+  };
+
+  const onClick = async (tabName) => {
+    setDropdown(tabName);
+    switch (tabName) {
+      case 'assignee':
+        setOptions(optionGenerator.users(await service.getUsers(), currentSelect.assignees));
+        break;
+      case 'label':
+        setOptions(optionGenerator.labels(await service.getLabels()));
+        break;
+      case 'milestone':
+        setOptions(optionGenerator.milestones(await service.getMilestones({})));
+        break;
+      default:
+        setOptions([]);
+    }
+  };
+
   return (
     <>
       <Wrapper>
-        <Title>
+        <Title
+          onClick={() => {
+            onClick('assignee');
+          }}
+        >
           <p>Assignee</p>
           <RiSettings4Line />
         </Title>
-        {currentSelect.user ? '' : 'No one'}
+        {dropdown === 'assignee' && (
+          <Dropdown
+            title='Assign up to 10 people to this issue'
+            toggleDropdown={toggleDropdown}
+            width='100%'
+            options={options}
+            defaultSelect={currentSelect.assignees}
+            onChange={(selected) => {
+              chageSelect({ type: 'assignee', newSelection: selected });
+            }}
+          />
+        )}
+        {currentSelect.assignees.length
+          ? currentSelect.assignees.map((selectedAssignee) => (
+              <div key={selectedAssignee}>{selectedAssignee}</div>
+            ))
+          : 'No one'}
       </Wrapper>
       <Wrapper>
-        <Title>
+        <Title
+          onClick={() => {
+            onClick('label');
+          }}
+        >
           <p>Label</p>
           <RiSettings4Line />
         </Title>
-        {currentSelect.label ? '' : 'None yet'}
+        {dropdown === 'label' && (
+          <Dropdown
+            title='Apply labels to this issue'
+            toggleDropdown={toggleDropdown}
+            width='100%'
+            options={options}
+            defaultSelect={currentSelect.labels}
+            onChange={(selected) => {
+              chageSelect({ type: 'label', newSelection: selected });
+            }}
+          />
+        )}
+        {currentSelect.labels.length
+          ? currentSelect.labels.map((selectedLabel) => <div key={selectedLabel}>{selectedLabel}</div>)
+          : 'None yet'}
       </Wrapper>
       <Wrapper>
-        <Title>
+        <Title
+          onClick={() => {
+            onClick('milestone');
+          }}
+        >
           <p>Milestone</p>
           <RiSettings4Line />
         </Title>
-        {currentSelect.label ? '' : 'No milestone'}
+        {dropdown === 'milestone' && (
+          <Dropdown title='Set milestone' toggleDropdown={toggleDropdown} width='100%' options={options} />
+        )}
+        {currentSelect.milestone ? '' : 'No milestone'}
       </Wrapper>
     </>
   );
 };
 
 const Wrapper = styled.div`
+  position: relative;
   padding: 20px 0;
   border-bottom: 1px solid gray;
   font-size: ${({ theme }) => theme.fontSize.sm};
