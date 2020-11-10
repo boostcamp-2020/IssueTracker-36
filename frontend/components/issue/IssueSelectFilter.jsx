336ad2c244a3e9d45f7c69c619bc7b7f8d240ed9
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2020-11-11 02:33:14 +0900

    [FE][feat] : 필터 옵션 클릭 동작(라벨 제외) #113 - 필터 클릭 시 필요한 동작 주입   - 클릭 시 id값을 전달하도록 OptionGenerator에서 설정 - 현재 query를 base로 새로운 query를 설정   - query의 author, milstone, assignee를 새로 설정한다   - 새로 생성한 query로 페이지 이동

--- frontend/components/issue/IssueSelectFilter.jsx
+++ frontend/components/issue/IssueSelectFilter.jsx
@@ -1,14 +1,49 @@
 import React, { useEffect, useState } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import qs from 'query-string';
 import Dropdown from '@components/common/Dropdown';
 import service from '@services';
 import optionGenerator from '@utils/OptionGenerator';
 import { RiArrowDownSFill } from 'react-icons/ri';
 
+const filterAuthor = (history, params, authorId) => {
+  const url = qs.stringifyUrl({
+    url: '/issues',
+    query: {
+      ...params,
+      author: authorId,
+    },
+  });
+  history.push(url);
+};
+const filterMilestone = (history, params, milestoneId) => {
+  const url = qs.stringifyUrl({
+    url: '/issues',
+    query: {
+      ...params,
+      milestone: milestoneId,
+    },
+  });
+  history.push(url);
+};
+const filterAssignee = (history, params, assigneeId) => {
+  const url = qs.stringifyUrl({
+    url: '/issues',
+    query: {
+      ...params,
+      assignee: assigneeId,
+    },
+  });
+  history.push(url);
+};
+
 const IssueSelectFilter = ({ filterName, dropdownTitle }) => {
   const [showDropdown, setShowDropdown] = useState(false);
   const [optionData, setOptionData] = useState([]);
+  const history = useHistory();
+  const location = useLocation();
 
   const toggleDropdown = () => {
     setShowDropdown(!showDropdown);
@@ -16,16 +51,33 @@ const IssueSelectFilter = ({ filterName, dropdownTitle }) => {
 
   useEffect(async () => {
     if (!showDropdown) return;
+    const params = qs.parse(location.search);
     switch (filterName) {
       case 'Label':
-        setOptionData(optionGenerator.labels(await service.getLabels()));
+        setOptionData(optionGenerator.labels(await service.getLabels(), []));
         break;
       case 'Author':
+        setOptionData(
+          optionGenerator.users(await service.getUsers(), [], filterAuthor.bind(undefined, history, params)),
+        );
+        break;
       case 'Assignee':
-        setOptionData(optionGenerator.users(await service.getUsers()));
+        setOptionData(
+          optionGenerator.users(
+            await service.getUsers(),
+            [],
+            filterAssignee.bind(undefined, history, params),
+          ),
+        );
         break;
       case 'Milestones':
-        setOptionData(optionGenerator.milestones(await service.getMilestones({})));
+        setOptionData(
+          optionGenerator.milestones(
+            await service.getMilestones({}),
+            [],
+            filterMilestone.bind(undefined, history, params),
+          ),
+        );
         break;
       default:
         setOptionData([]);
