59266a7d77d785cfbbcc9afae5361d87d0c6f03d
mjseok <mjseok98@gmail.com>
2020-11-10 14:54:33 +0900

    [BE][feat] : 마일스톤 api 라우팅  #109,#115
    마일스톤 수정,삭제 api 추가

--- backend/routes/milestone/index.js
+++ backend/routes/milestone/index.js
@@ -8,7 +8,10 @@ router.get('/milestones', service.getMilestones);
 router.get('/milestone/:id', service.getMilestone);
 
 // post
-// put
+// patch
+router.patch('/milestone/:id', service.editMilestone);
+
 // delete
+router.delete('/milestone/:id', service.deleteMilestone);
 
 module.exports = router;
