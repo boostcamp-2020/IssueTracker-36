c5aeb22d31d477695267734ae7200f5f28473a23
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2020-11-10 15:32:32 +0900

    [BE][feat] : 마일스톤 추가 api #108 - 마일스톤 추가 API 구현

---
+++ backend/services/milestone/addMilestone.js
@@ -0,0 +1,16 @@
+const { milestone } = require('../../sequelize/models');
+
+module.exports = async (req, res) => {
+  try {
+    const { title, dueDate, description } = req.body;
+    const result = await milestone.create({
+      title,
+      dueDate,
+      description,
+      isClosed: false,
+    });
+    res.json(result);
+  } catch (e) {
+    res.sendStatus(500);
+  }
+};
