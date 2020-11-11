c190d2b33fca7d525f1ac0ec77808154d8287a27
dogyeong <shroad1802@naver.com>
2020-11-11 22:12:03 +0900

    [BE][feat] : reaction 추가/삭제 API 추가
    - 리액션 추가
    - 리액션 삭제

--- backend/routes/comment/index.js
+++ backend/routes/comment/index.js
@@ -5,11 +5,13 @@ const service = require('../../services').comment;
 
 // post
 router.post('/comment', service.addComment);
+router.post('/comment/:id/reaction', service.addReaction);
 
 // patch
 router.patch('/comment/:id', service.updateComment);
 
 // delete
 router.delete('/comment/:id', service.deleteComment);
+router.delete('/comment/:cid/reaction/:rid', service.deleteReaction);
 
 module.exports = router;
