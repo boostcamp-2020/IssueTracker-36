51ed4c7d4f17e48b67b1a38de4167f1090e2bc83
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2020-11-11 21:38:17 +0900

    [FE][feat] : 마일스톤 관련 dispatch 추가 - 마일스톤 업데이트, close 관련 type 추가 - 마일스톤 추가 dispatch 로직 수정

--- frontend/store/MilestoneProvider.jsx
+++ frontend/store/MilestoneProvider.jsx
@@ -15,16 +15,39 @@ const reducer = (state, action) => {
         close: [...state.close, ...payload.close],
       };
     case milestoneActions.ADD_MILESTONE:
-      const { isClosed } = payload;
       return {
-        open: isClosed ? [...state.open] : [...state.open, ...payload],
-        close: isClosed ? [...state.close, ...payload] : [...state.close],
+        ...state,
+        open: [...state.open, payload],
       };
     case milestoneActions.DELETE_MILESTONE:
       return {
         open: [...state.open.filter((milestone) => milestone.id !== payload)],
         close: [...state.close.filter((milestone) => milestone.id !== payload)],
       };
+    case milestoneActions.UPDATE_MILESTONE:
+      return {
+        open: state.open.reduce((acc, milestone) => {
+          acc.push(milestone.id === payload.id ? { ...milestone, ...payload } : milestone);
+          return acc;
+        }, []),
+        close: state.close.reduce((acc, milestone) => {
+          acc.push(milestone.id === payload.id ? { ...milestone, ...payload } : milestone);
+          return acc;
+        }, []),
+      };
+    case milestoneActions.CHANGE_CLOSED:
+      const originalMilestone = payload.isClosed ? state.close : state.open;
+      const movingMilestone = originalMilestone.find((milestone) => milestone.id === payload.id);
+      movingMilestone.isClosed = !payload.isClosed;
+      return payload.isClosed
+        ? {
+            open: [...[...state.open, movingMilestone].sort((first, second) => first.id - second.id)],
+            close: [...state.close.filter((milestone) => milestone.id !== payload)],
+          }
+        : {
+            open: [...state.open.filter((milestone) => milestone.id !== payload.id)],
+            close: [...[...state.close, movingMilestone].sort((first, second) => first.id - second.id)],
+          };
     default:
       return state;
   }
