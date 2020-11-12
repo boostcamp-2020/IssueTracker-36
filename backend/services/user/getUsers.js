242d38647cec9f27944987527c63a7903bc7ff4f
saeeng <saeeng.op@gmail.com>
2020-11-12 17:53:48 +0900

    [BE][feat] user 테이블 img 추가
    유저 아바타 저장용 컬럼 추가

--- backend/services/user/getUsers.js
+++ backend/services/user/getUsers.js
@@ -2,7 +2,7 @@ const { user } = require('../../sequelize/models');
 
 module.exports = async (req, res) => {
   try {
-    const result = await user.findAll({ attributes: ['id', 'nickName'] });
+    const result = await user.findAll({ attributes: ['id', 'nickName', 'img_url'] });
     const requestUserIndex = result.findIndex((row) => row.id === req.body.uid);
     result.splice(0, 0, result.splice(requestUserIndex, 1)[0]);
     res.json(result);
