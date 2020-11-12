1e194620ebfb1790b1a5159d5f69115d959c3c46
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2020-11-12 15:22:57 +0900

    [BE][feat] : 이슈 상세 페이지에서 사이드바 관련 API 추가 - 마일스톤 추가 및 삭제 - 함수명 수정

---
+++ backend/services/issue/addIssueMilestone.js
@@ -0,0 +1,13 @@
+const { issue } = require('../../sequelize/models');
+
+const addIssueMilestone = async (req, res) => {
+  try {
+    const { issueId, milestoneId } = req.params;
+    const result = await issue.update({ milestoneId }, { where: { id: issueId } });
+    res.json(result);
+  } catch (err) {
+    res.sendStatus(500);
+  }
+};
+
+module.exports = addIssueMilestone;
