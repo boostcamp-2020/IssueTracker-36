d82da4c3fe90afa3b2c632a1619bf59f2ebe4b7a
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2020-11-12 16:03:20 +0900

    [Common][fix] : 이슈 추가 관련 문제 수정 - 마일스톤이 없을 경우 오류가 생기는 버그 수정 - WritingArea 모듈 에러 수정

--- frontend/components/common/WritingArea.jsx
+++ frontend/components/common/WritingArea.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import ReactMarkdown from 'react-markdown';
+import service from '@services';
 
 window.process = { cwd: () => '' };
 
